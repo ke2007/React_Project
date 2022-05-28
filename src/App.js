1cd6dd2144b9c89cc26b98f3264081c443c593bd
dlawn <dlawnsrjs@gmail.com>
2022-05-28 15:48:58 +0900

    # UseEffect 사용연습🦝

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import Button from "./Button";
+import Appstyles from "./App.module.css";
+import Btnstyles from "./Button.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeword] = useState("");
+  const onk = () => setValue((sum) => sum + 1);
+  const onChange = (event) => setKeword(event.target.value);
+
+  useEffect(() => {
+    console.log("I run only once, like API");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' add five times.", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
-      <h1>Welcome Back!!!</h1>
-      <Button text={"Continue"} />
+      <h3 className={Appstyles.title}>Welcome Back!!!!</h3>
+      <hr></hr>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h2>{counter}</h2>
+      <Button text={"Continue~!"} />
+      <button onClick={onk} className={Btnstyles.btn}>
+        clickme
+      </button>
     </div>
   );
 }
